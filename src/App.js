3470b9102d5840c4f2be99699aec91946d8509d8
Naice <jeffry.sandy99@gmail.com>
2025-01-19 22:51:14 +0700

    using variable inside JSX

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 import React from 'react'
 
 export default function App() {
+
+    const username = "Naice";
   return (
-    <div>App</div>
+    <>
+        <h1 className='active'> {username} </h1>
+        <br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, labore beatae. Nemo veniam, fugit laudantium natus fuga doloribus, perferendis distinctio eum, in molestiae quam? Quidem ipsam provident quibusdam distinctio laborum sit, officiis, consequuntur magni perspiciatis reiciendis sequi, nam eos. Voluptatibus molestiae tempora hic similique dicta exercitationem earum corrupti consequatur, eveniet consequuntur pariatur velit neque maiores consectetur optio reiciendis aut blanditiis illo maxime dolorum repellat! Laudantium facilis magnam vel corporis numquam labore neque esse, cupiditate accusamus, similique ipsum iste inventore hic est illo. Autem voluptate ducimus necessitatibus alias atque blanditiis ut unde asperiores delectus? Voluptate vel veritatis dignissimos? Exercitationem, nobis excepturi.</p>
+    </>
   )
 }
