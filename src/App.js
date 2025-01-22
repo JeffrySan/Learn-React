4641765eb2de50ca0eb309e882dcdee51e4d9c26
Naice <jeffry.sandy99@gmail.com>
2025-01-22 10:42:24 +0700

    using image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 
 import React from 'react'
+import Header from './components/Header'
+import Footer from "./components/Footer"
 
 export default function App() {
 
-    const username = "Naice";
-  return (
-    <>
-        <h1 className='active'> {username} </h1>
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, labore beatae. Nemo veniam, fugit laudantium natus fuga doloribus, perferendis distinctio eum, in molestiae quam? Quidem ipsam provident quibusdam distinctio laborum sit, officiis, consequuntur magni perspiciatis reiciendis sequi, nam eos. Voluptatibus molestiae tempora hic similique dicta exercitationem earum corrupti consequatur, eveniet consequuntur pariatur velit neque maiores consectetur optio reiciendis aut blanditiis illo maxime dolorum repellat! Laudantium facilis magnam vel corporis numquam labore neque esse, cupiditate accusamus, similique ipsum iste inventore hic est illo. Autem voluptate ducimus necessitatibus alias atque blanditiis ut unde asperiores delectus? Voluptate vel veritatis dignissimos? Exercitationem, nobis excepturi.</p>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <h1 className='active'> Naice </h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, labore beatae. Nemo veniam, fugit laudantium natus fuga doloribus, perferendis distinctio eum, in molestiae quam? Quidem ipsam provident quibusdam distinctio laborum sit, officiis, consequuntur magni perspiciatis reiciendis sequi, nam eos. Voluptatibus molestiae tempora hic similique dicta exercitationem earum corrupti consequatur, eveniet consequuntur pariatur velit neque maiores consectetur optio reiciendis aut blanditiis illo maxime dolorum repellat! Laudantium facilis magnam vel corporis numquam labore neque esse, cupiditate accusamus, similique ipsum iste inventore hic est illo. Autem voluptate ducimus necessitatibus alias atque blanditiis ut unde asperiores delectus? Voluptate vel veritatis dignissimos? Exercitationem, nobis excepturi.</p>
+            <Footer />
+        </>
+    )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import Blender from "./../assets/blender.JPG"
+import "./header.css"
+export default function Header() {
+  return (
+    <>
+        <img className="blender" src={Blender} alt=""/>
+        <div>Header</div>
+    </>
+  )
+}
